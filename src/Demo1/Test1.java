bc030ed6fcd1235ebfe620f69842acb6bb9b2c11
Hitesh Mane <Hitesh Mane@DESKTOP-P66OI27>
2022-10-31 22:49:02 +0530

    First test case1

---
+++ src/Demo1/Test1.java
@@ -0,0 +1,15 @@
+package Demo1;
+
+public class Test1
+{
+public static void main(String[] args) 
+{
+
+	System.out.println("Good Morning");
+	m1();
+}
+	public static void m1()
+	{
+		System.out.println("Hi");
+	}
+}
